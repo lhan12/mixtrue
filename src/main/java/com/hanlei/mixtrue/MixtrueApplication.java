326be419be63722c33202989f4b4caa0429a34bb
hanlei <hanlei0@gaosiedu.com>
2020-01-13 15:49:23 +0800

    fist commit

---
+++ src/main/java/com/hanlei/mixtrue/MixtrueApplication.java
@@ -0,0 +1,13 @@
+package com.hanlei.mixtrue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixtrueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixtrueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanlei/mixtrue/MixtrueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanlei.mixtrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixtrueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
